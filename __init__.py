d6b926cacaf1649ad6953eb66ce1d3d3eceeed23
iZerat <1037789522@qq.com>
2023-10-13 16:27:44 +0800

    添加了 uilist 的部分

--- __init__.py
+++ __init__.py
@@ -8,18 +8,28 @@ bl_info = {
     "warning" : "",
     "category" : "Render"
 }
-
+0
 
 import bpy
 import os
 from bpy.props import EnumProperty, BoolProperty, StringProperty, IntProperty, FloatProperty, PointerProperty
 
+
+class MyItem(bpy.types.PropertyGroup):
+    name:bpy.props.StringProperty(name = "name", default = "")
+    data:bpy.props.StringProperty(name = "name", default = "")
+
+
+
 # 定义属性类
-class My_Properties(bpy.types.PropertyGroup):
+def My_Properties():
 
-    blbl_int1: bpy.props.IntProperty(name = "blbl_int1",soft_min = 0)
+    # list 数据
+    bpy.types.Scene.myList = bpy.props.CollectionProperty(type = MyItem)
 
-    blbl_myBool_poweroff: bpy.props.BoolProperty(name = "myBool_poweroff")
+    # 当前选中的下标
+    bpy.types.Scene.active_index = bpy.props.IntProperty(name = "current index", default = 0, min = 0)
+
 
 
 
@@ -55,7 +65,7 @@ def cmd_Run():
 
 # 侧边栏
 class RenderingGear_OperatorUI(bpy.types.Panel):
-    bl_idname = 'wm.rendering_gear_ui'
+    bl_idname = 'iz.rendering_gear_ui'
     bl_label = '后台自动渲染'
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
@@ -69,41 +79,135 @@ class RenderingGear_OperatorUI(bpy.types.Panel):
         
         # 按钮
         row2 = layout.row()
-        row2.operator("wm.rendering_gear", text='开始渲染', icon='DRIVER')
+        row2.operator("iz.rendering_gear1", text='开始渲染', icon='DRIVER')
 
-        row3 = layout.row()
-        row3.prop(context.scene.my_properties,"myBool_poweroff", text="这是一个单选框")
         
 
 
-# 执行操作
-class RenderingGear_Operator(bpy.types.Operator):
-    bl_idname = 'wm.rendering_gear'
-    bl_label = 'RenderingGearOperator'
+# 执行操作1
+class RenderingGear_OT_Operator1(bpy.types.Operator):
+    bl_idname = 'iz.rendering_gear1'
+    bl_label = 'RenderingGearOperator1'
 
     # 执行的内容
     def execute(self, context):         
         command = cmd_Run()
         os.system(command) # 给终端打命令
-        # os.system("shutdown -s -t 60")
         return {'FINISHED'}
 
 
 
 
+
+
+# 创建队列
+class RenderingGear_OT_CreateItem(bpy.types.Operator):
+    bl_idname = 'iz.rendering_createitem'
+    bl_label = 'RenderingGearCreateItem'
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+
+    # 执行的内容
+    def execute(self, context):         
+
+        return {'FINISHED'}
+
+# 删除队列
+class RenderingGear_OT_DeleteItem(bpy.types.Operator):
+    bl_idname = 'iz.rendering_deleteitem'
+    bl_label = 'RenderingGearDeleteItem'
+
+    @classmethod
+    def poll(cls, context):
+        return True
+    
+    # 执行的内容
+    def execute(self, context):         
+
+        return {'FINISHED'}
+
+# 移动队列
+class RenderingGear_OT_MoveItem(bpy.types.Operator):
+    bl_idname = 'iz.rendering_moveitem'
+    bl_label = 'RenderingGearMoveItem'
+
+    @classmethod
+    def poll(cls, context):
+        return True
+    
+    # 执行的内容
+    def execute(self, context):         
+
+        return {'FINISHED'}
+
+# 自定义uilist
+class RenderingGear_UL_Queue(bpy.types.UIList):
+    def draw_item(self, context, layout, data, item, icon_value, active_data, active_propname, index):
+
+        icon = "BLENDER"
+
+        # 三种显示模式: 'DEFAULT', 'COMPACT', 'GRID'
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            layout.label(text = "", translate = False, icon = icon)
+
+        elif self.layout_type in {'GRID'}:
+            layout.alignment = 'CENTER'
+            layout.label(text = "", icon = icon)
+
+        pass
+
+# 显示队列
+class RenderingGear_PT_Queue(bpy.types.Panel):
+    bl_idname = 'iz.rendering_queue'
+    bl_label = 'RenderingGearQueue'
+
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'UIList'
+
+    def draw(self, context):
+        layout = self.layout
+
+        scene = context.scene
+
+        row1 = layout.row()
+
+        row1.template_list("RenderingGear_UL_Queue", "test list", scene, "myList", scene, "active_index")
+        pass
+
+
+
+
 # 注册与注销
+
+classes = [
+    MyItem,
+    RenderingGear_OperatorUI,
+    RenderingGear_OT_Operator1,
+    RenderingGear_OT_CreateItem,
+    RenderingGear_OT_MoveItem,
+    RenderingGear_UL_Queue,
+    RenderingGear_PT_Queue
+]
 def register():
-    bpy.utils.register_class(My_Properties)
-    bpy.utils.register_class(RenderingGear_Operator)
-    bpy.utils.register_class(RenderingGear_OperatorUI)
+    for item in classes:
+        bpy.utils.register_class(item) 
+
+    # 注册属性
+    My_Properties()
+
     print("register complex")
 
 
 def unregister():
-    bpy.utils.unregister_class(My_Properties)
-    bpy.utils.unregister_class(RenderingGear_Operator)
-    bpy.utils.unregister_class(RenderingGear_OperatorUI)
+    for item in classes:
+        bpy.utils.unregister_class(item)    
+
     print("see you")
 
-bpy.types.Scene.my_properties = bpy.props.PointerProperty(type = My_Properties)
+
+
 
